d8655e4941be5d555c5476a4ca76f279c1cc5ecc
adolaher <addolaher@gmail.com>
2022-01-12 10:03:58 -0500

    finish tests

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -57,7 +57,8 @@ beforeEach(async () => {
     assert.equal(accounts[2], players[2]);
     assert.equal(3, players.length)
   });
-  it('requires a minimum amoutn of ether to enter', async () => {
+
+  it('requires a minimum amount of ether to enter', async () => {
     try {
       await lottery.methods.enter().send({
         from: accounts[0],
@@ -67,5 +68,28 @@ beforeEach(async () => {
     } catch (err) {
       assert(err);
     }
+  });
+  it('only manager can call pickwinner', async() => {
+    try{
+      await lottery.methods.pickWinner().send({
+        from: accounts[1]
+      });
+      assert(false);
+    } catch (err) {
+      assert(err);
+    }
+  });
+
+  it('send money to the winner and resets player array', async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei('2', 'ether')
+    })
+    const initialBalance = await web3.eth.getBalance(accounts[0]);
+    await lottery.methods.pickWinner().send({ from: accounts[0]});
+    const finalBalance = await web3.eth.getBalance(accounts[0]);
+    const difference = finalBalance - initialBalance;
+    
+    assert(difference > web3.utils.toWei('1.8', 'ether'));
   });
 });
